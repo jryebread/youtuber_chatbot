22469e5bbd1d8e1d544fd76eaac737cd160237f2
jryebread <jriback144@gmail.com>
2023-02-10 17:46:08 -0800

    openai

--- main.py
+++ main.py
@@ -10,6 +10,8 @@ from langchain.chains import ChatVectorDBChain
 from langchain.prompts import PromptTemplate
 from pathlib import Path
 import os
+import openai
+
 load_dotenv()
 import streamlit as st
 OPENAI_KEY = os.getenv('OPENAI_KEY')
